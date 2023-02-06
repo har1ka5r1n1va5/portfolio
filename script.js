57de26842d9cba5beb6bc5e56d1fbb0865351874
harika <harikamanapurapu1010@gmail.com>
2023-02-06 14:21:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+let themeElement=document.getElementById("theme1")
+function themebtn(){
+    themeElement.classList.toggle("dark")
+}
